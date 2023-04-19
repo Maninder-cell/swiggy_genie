d138cee10874d3306f4f825d299f7a5126d2298d
Shadab Hassan <sadabia854318@gmail.com>
2023-04-19 12:04:00 +0530

    First Commit

--- routes/task.js
+++ routes/task.js
@@ -4,7 +4,9 @@ var taskcontroller = require('../controller/taskController');
 
 router.post('/upload-amenity',upload.array("amenity"),taskcontroller.addAmenity);
 
-router.post('/get-amenity',taskcontroller.getAmenities)
+router.get('/get-amenity',taskcontroller.getAmenities);
+
+router.delete('/delete-amenity',taskcontroller.deleteAmenities);
 
 module.exports = router;
 
