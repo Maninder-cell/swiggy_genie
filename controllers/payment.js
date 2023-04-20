bab8d458668e4c8ebea0ccdb8557df9115578e3f
manindermatharu2001 <manindermatharu2001@gmail.com>
2023-04-20 10:00:35 +0530

    stripe payment added

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,3 +1,43 @@
-exports.payment = (req,res,next) => {
-    return res.send('mskf');
-}
+require("dotenv").config();
+
+const stripe = require("stripe")(process.env.stripe_secret_key);
+
+exports.createCustomer = async (req, res, next) => {
+  const paymentMethod = await stripe.paymentMethods.create({
+    type: "card",
+    card: {
+      number: "4242424242424242",
+      exp_month: 12,
+      exp_year: 2024,
+      cvc: "987",
+    },
+  });
+
+  const customer = await stripe.customers.create({
+    name: "Maninder",
+    email: "manindermatharu2001@gmail.com",
+    payment_method: paymentMethod.id,
+    invoice_settings: {
+      default_payment_method: paymentMethod.id
+    }
+  });
+
+  return res.status(200).json({
+    customer: customer,
+  });
+};
+
+exports.pay = async (req, res, next) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 100 * 100000,
+    currency: "usd",
+    confirm: true,
+    customer: "cus_Nk47G3wCRLsJrV",
+    payment_method: "pm_1MyZziJC7o9g4pD4LJiBiQaX",
+    payment_method_types: ["card"],
+  });
+
+  return res.status(200).json({
+    payment: paymentIntent,
+  });
+};
--- routes/payment.js
+++ routes/payment.js
@@ -4,9 +4,7 @@ const { body } = require("express-validator");
 
 const router = express.Router();
 
-router.post(
-    "/new",
-    paymentController.payment
-);
+router.post("/create_customer",paymentController.createCustomer);
+router.post("/pay",paymentController.pay);
 
 module.exports = router;
