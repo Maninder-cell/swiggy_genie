7a9dc4e44d2e0e7fa8a267778fdc6a11a05b9381
Shadab Hassan <58871340+Naushadia@users.noreply.github.com>
2023-04-28 17:26:11 +0530

    Update profileController.js

--- Controllers/profileController.js
+++ Controllers/profileController.js
@@ -3,10 +3,6 @@ const models = require("../models");
 const User = models.User;
 
 const { validationResult } = require("express-validator");
-const models = require("../models");
-
-
-const { validationResult } = require("express-validator");
 
 exports.editprofileController = async (req, res) => {
     const errors = validationResult(req);
@@ -15,9 +11,12 @@ exports.editprofileController = async (req, res) => {
     }
     try {
       const updatedUser = {...req.body};
+      console.log(updatedUser);
+      // const find = await User.findOne({where:{Phone:updatedUser.Phone}});
       const find = await User.findByPk((req.user.id));
       if (!find) {
-        return res.status(201).json({ Message:"User Not Found" });
+        const user = await User.create(updatedUser);
+        return res.status(201).json({ user,Message:"User Created Sucessfully" });
       } else {
         const user = await find.update(updatedUser,{returning: true, attributes: ['Name','photoUri','Address','Email','Phone','status']});
         return res.status(201).json({ user,Message:"User Updated Sucessfully" });
