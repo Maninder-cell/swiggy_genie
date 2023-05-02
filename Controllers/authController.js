3c7da6e5bc68e248cf092b132475821174252de8
Shadab Hassan <58871340+Naushadia@users.noreply.github.com>
2023-05-02 11:54:58 +0530

    Update authController.js

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const register = async (req, res) => {
   try {
-    const { phoneNumber } = req.body;
+    const { phoneNumber,CallingCode } = req.body;
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -22,7 +22,7 @@ const register = async (req, res) => {
     }
 
     //find user
-    const userExists = await User.findOne({ where: { Phone: phoneNumber } });
+    const userExists = await User.findOne({ where: { Phone:phoneNumber } });
 
     if (userExists) {
       return res
@@ -32,15 +32,12 @@ const register = async (req, res) => {
 
     const newUser = await User.create({
       Phone: phoneNumber,
+      CallingCode: CallingCode
     });
 
     //generate token
     const token = jwt.sign(
-      {
-        phoneNumber: newUser.Phone,
-        id: newUser.id,
-        role: newUser.account_type,
-      },
+      { phoneNumber: newUser.Phone, id: newUser.id, role: newUser.account_type },
       process.env.JWT_SECRET,
       {
         expiresIn: "15d",
@@ -51,19 +48,19 @@ const register = async (req, res) => {
     newUser.lastLoggedIn = Date.now();
     await newUser.save();
 
-    // Set token as cookie
-    res.cookie("auth_token", token, {
-      maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-      httpOnly: true,
-      // secure: process.env.NODE_ENV === "production",
-    });
+        // Set token as cookie
+        res.cookie("auth_token", token, {
+          maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+          httpOnly: true,
+          // secure: process.env.NODE_ENV === "production",
+        });
     return res.status(200).json({
       success: true,
       msg: "User created successfully",
       data: {
         user_id: newUser.id,
         Phone: newUser.Phone,
-        // Token:newUser.tokens
+        token: token,
       },
     });
   } catch (err) {
@@ -77,7 +74,7 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   try {
     const { phoneNumber } = req.body;
-
+    console.log(phoneNumber);
     //validation error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -85,7 +82,7 @@ const login = async (req, res) => {
     }
 
     //find user by PhoneNumber
-    const user = await User.findOne({ where: { Phone: phoneNumber } });
+    const user = await User.findOne({ where: { Phone:phoneNumber } });
 
     if (!user) {
       return res
